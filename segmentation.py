11dcdd62ab2b5a36c0badeea0df103c55752deb3
cornellpolarbear <ba345@cornell.edu>
2018-12-08 14:53:25 -0500

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -4,7 +4,8 @@ import tensorflow as tf
 
 import matplotlib.pyplot as plt
 #%matplotlib inline # Only use this if using iPython
-image_index = 7778 # You may select anything up to 60,000
+#every index is a digit
+image_index = 7777 # You may select anything up to 60,000
 print(y_train[image_index]) # The label is 8
 plt.imshow(x_train[image_index], cmap='Greys')
 plt.show()
