8d823f6b54df139f3e3cc87373bfb995271a11c1
ba345 <ba345@cornell.edu>
2018-12-10 21:47:46 -0500

    Bill's changes v2

--- imagePrac.py
+++ imagePrac.py
@@ -1,22 +1,28 @@
 from scipy import misc
 import numpy as np
+import data_process as dp
 #face = misc.face()
 #misc.imsave('face.png', face) # First we need to create the PNG file
 
-face = misc.imread('2.jpg')
+#face = misc.imread('2.jpg')
 #type(face)      
 
-face.shape, face.dtype
+#face.shape, face.dtype
 import matplotlib.pyplot as plt
-plt.imshow(face)
-plt.show()
+#plt.imshow(face)
+#plt.show()
 #print(type(face[0][0][0]))
-print(face)
-""
-for val in face:
+#print(face)
+#x = dp.load_image('pixelshot.jpeg')/255.0
+x = dp.load_image('hand1.jpg')
+plt.imshow(x, cmap='Greys')
+plt.show()
+print(x[0][0])
+for val in x:
     row = val[0]
     col = val[1]
     rbg = val[2]
-    if (np.array_equal(rbg, [255, 255, 255])):
-        print(rbg)
+    #if (np.array_equal(rbg, [255, 255, 255])):
+    #if(rbg.all() >= 0):
+        #print('row: {}, col: {}'.format(row, col))
 
